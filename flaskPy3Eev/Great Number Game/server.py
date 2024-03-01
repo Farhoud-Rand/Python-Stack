0cc9f1290a0f33a19c214d38ebb5ff8fa005c1ee
Rand <r.farhoud2000@gmail.com>
2024-03-01 12:54:28 +0200

    flask assignment third version

--- flaskPy3Eev/Great Number Game/server.py
+++ flaskPy3Eev/Great Number Game/server.py
@@ -4,37 +4,68 @@ from flask import Flask, render_template, redirect, session, request
 app = Flask(__name__)
 app.secret_key = "my_key"
 
-# Our index route will handle rendering our refeashing the page
+# This page used to select the game difficulty  
 @app.route('/')
-def index():
-    # Check if 'number' key exists in session
-    if 'number' not in session :
+def welcome():
+    return render_template("welcome.html")
+
+# Easy game
+@app.route('/easy')
+def easy():
+    if 'number' not in session : # Check if 'number' key exists in session
         session['number'] = random.randint(1, 100) 	# random number between 1-100
-    if 'tries' not in session :
+    if 'tries' not in session : # Check if 'tries' key exists in session
         session['tries'] = 0
     print(session)
     print("*"*80)
     sys.stdout.flush() # To ensure that the print statements are immediately visible in the terminal while the Flask server is running, you can explicitly flush the output buffer after each print statement using sys.stdout.flush().
-    return render_template("index.html")
+    return render_template("easy.html")
 
-# Increment the number of visits by spcific number
+# Hard game
+@app.route('/hard')
+def hard():
+    if 'number' not in session : # Check if 'number' key exists in session
+        session['number'] = random.randint(1, 100) 	# random number between 1-100
+    if 'tries' not in session : # Check if 'tries' key exists in session
+        session['tries'] = 0
+    if 'limit' not in session : # Check if 'limit' key exists in session
+        session['limit'] = 5
+    print(session)
+    print("*"*80)
+    sys.stdout.flush() # To ensure that the print statements are immediately visible in the terminal while the Flask server is running, you can explicitly flush the output buffer after each print statement using sys.stdout.flush().
+    return render_template("hard.html")
+
 @app.route('/guess_from_user', methods=['POST'])
 def check_number():
-    session['tries'] += 1
-    user_number = int(request.form['user_number']) # Convert the number to integer first
-    if (user_number == session['number']):                             # Case 1: Success
-        return render_template("success.html")
-    elif (session['number']- 10 <= user_number < session['number'] ):  # Case 2: too close but lower than number by 10 or less
+    # Check the limitation counter for hard game
+    if 'limit' in session :
+        if (session['limit'] > 0):
+            session['limit'] -= 1
+    session['tries'] += 1 # Increment the number of visits by spcific number
+    session['user_number'] = int(request.form['user_number']) # Convert the number to integer first
+    if (session['number']- 10 <= session['user_number'] < session['number'] ):  # Case 2: too close but lower than number by 10 or less
         session['result'] = 'Low'
-    elif (session['number']  < user_number <= session['number'] + 10): # Case 3: too close but higher than number by 10 or less
+    elif (session['number']  < session['user_number'] <= session['number'] + 10): # Case 3: too close but higher than number by 10 or less
         session['result'] = 'High'
-    elif (user_number > session['number']):                            # Case 4: too far from number from above 
+    elif (session['user_number'] > session['number']):                            # Case 4: too far from number from above 
         session['result'] = 'Too high !'
     else:                                                              # Case 5: too far from number from below
         session['result'] = 'Too low !'
-    return render_template("fail.html")        
-        
-
+    return redirect('/show_result')
+           
+# show result (because if I use render-template in check-number function it will decrease the numbers when we refresh the page)
+@app.route('/show_result')
+def show():
+    if ('limit' in session and session['limit'] == 0): # Check if last attempt was correct (hard game)
+        if (session['user_number'] == session['number']):
+            return render_template("success.html")
+        else:
+            return render_template ("limit_fail.html") # Fail case (hard game)
+    if (session['user_number'] == session['number']): # Case 1: Success (easy game)
+        return render_template("success.html")
+    else:
+        return render_template("fail.html") # Fail case (easy game)
+    
 # Clear the session 
 @app.route('/destroy_session')
 def clear_session():
