096024eb05ef01e41528980338dc1342cd62d140
Rand <r.farhoud2000@gmail.com>
2024-03-01 05:22:42 +0200

    flask assignment first version

---
+++ flaskPy3Eev/Great Number Game/server.py
@@ -0,0 +1,38 @@
+import sys
+import random # import the random module
+from flask import Flask, render_template, redirect, session, request
+app = Flask(__name__)
+app.secret_key = "my_key"
+
+# Our index route will handle rendering our refeashing the page
+@app.route('/')
+def index():
+    # Check if 'number' key exists in session
+    if 'number' not in session :
+        session['number'] = random.randint(1, 100) 	# random number between 1-100
+    print(session)
+    print("*"*80)
+    sys.stdout.flush() # To ensure that the print statements are immediately visible in the terminal while the Flask server is running, you can explicitly flush the output buffer after each print statement using sys.stdout.flush().
+    return render_template("index.html")
+
+# Increment the number of visits by spcific number
+@app.route('/guess_from_user', methods=['POST'])
+def check_number():
+    user_number = int(request.form['user_number']) # Convert the number to integer first
+    if (user_number == session['number']):
+        return render_template("success.html")
+    elif (user_number > session['number']):
+        session['result'] = 'high'
+    else:
+        session['result'] = 'low'
+    return render_template("fail.html")        
+        
+
+# Clear the session 
+@app.route('/destroy_session')
+def clear_session():
+    session.clear() # clears all keys
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
