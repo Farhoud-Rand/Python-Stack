8b12b054863fec4d466841ee0f941cc1ad5fa852
Rand <r.farhoud2000@gmail.com>
2024-03-14 16:29:27 +0200

    Django assignment second version

--- djangoPy3Env/ninja_gold/app1ninja/models.py
+++ djangoPy3Env/ninja_gold/app1ninja/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+def check_target(request):
+        # Check if the user reached the target gold
+        if request.session['total_golds'] >= request.session['target_gold']:
+            request.session['show_alert'] = True  # Set flag to show the alert
+            request.session['message'] = "Congratulations! You have reached 100 gold!"
+            request.session['result'] = 'success'
+        elif request.session['attempts'] == 0 and request.session['total_golds'] < request.session['target_gold']:
+            request.session['show_alert'] = True  # Set flag to show the alert
+            request.session['message'] = "Oops! You have run out of attempts. Try again!"
+            request.session['result'] = 'error'
--- djangoPy3Env/ninja_gold/app1ninja/views.py
+++ djangoPy3Env/ninja_gold/app1ninja/views.py
@@ -2,21 +2,32 @@ from django.shortcuts import render, redirect
 import random
 import sys
 from datetime import datetime
+from . import models
 
 # Root route will show the index.html page 
 def index(request):
     # Define the keys in the session
     if 'total_golds' not in request.session:
-        request.session['total_golds'] = 0 # This key will save the total number of golds the user have 
+        request.session['total_golds'] = 0    # This key will save the total number of golds the user have 
     if 'activities' not in request.session:
-        request.session['activities'] = [] # This key will save the list of user submittion activities
+        request.session['activities'] = []    # This key will save the list of user submittion activities
     if 'show_alert' not in request.session:
         request.session['show_alert'] = False # This key will save the list of user submittion activities
-    
+    if 'attempts' not in request.session:
+        request.session['attempts'] = 10      # Number of attempts allowed
+    if 'target_gold' not in request.session:
+        request.session['target_gold'] = 100  # Target gold to reach
+    if 'message' not in request.session:
+        request.session['message'] = ''       # Message in alart
+    if 'result' not in request.session:
+        request.session['result'] = ''        # Type of alart
+
     context = {
         'total_golds':request.session['total_golds'],
         'activities':request.session['activities'],
-        'show_alert':request.session['show_alert']
+        'show_alert':request.session['show_alert'],
+        'message':request.session['message'],
+        'result':request.session['result']
     }
     return render(request,"index.html", context)
 
@@ -49,10 +60,15 @@ def process_money(request):
         else:
             output = f"Entered a casino and lost {abs(random_number)} golds... Ouch.. ({formatted_time})" 
         request.session['activities'].insert(0,output) # Add the new activity (note use insert function instead of append in order to put the newest activity in the top)
-        return redirect("/")      # Redirect the root route 
+        
+        # Decrease the number of attempts
+        request.session['attempts'] -= 1
+        models.check_target(request)
+    
+        return redirect("/") # Redirect the root route 
 
 # Clear the session 
 def clear_session(request):
-    # del request.session # clears all keys
-    request.session.clear()
+    # del request.session 
+    request.session.clear()  # clears all keys
     return redirect("/")
