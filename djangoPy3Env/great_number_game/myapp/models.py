5e77258a75cfe5c174410f2369b17b6f17c761cd
Rand <r.farhoud2000@gmail.com>
2024-03-20 19:59:55 +0200

    django assignment second version

--- djangoPy3Env/great_number_game/myapp/models.py
+++ djangoPy3Env/great_number_game/myapp/models.py
@@ -1,3 +1,72 @@
 from django.db import models
+import sys
+import random # import the random module
 
-# Create your models here.
+# Function to intialize session  
+def initialize_session(request):
+    if 'number' not in request.session :                        # Check if 'number' key exists in session
+            request.session['number'] = random.randint(1, 100) 	# random number between 1-100
+    if 'tries' not in request.session :                         # Check if 'tries' key exists in session
+            request.session['tries'] = 0
+    print(request.session['number'])
+    print("*"*80)
+    sys.stdout.flush() # To ensure that the print statements are immediately visible in the terminal while the server is running, you can explicitly flush the output buffer after each print statement using sys.stdout.flush().
+#________________________________________________________________________________________________________
+def initialize_session_hard_game(request):
+    if 'winners' not in request.session:
+        request.session['winners'] = []
+    if 'limit' not in request.session :                     # Check if 'limit' key exists in session
+        request.session['limit'] = 5
+        request.session['type'] = 'hard'
+    print(request.session)
+    print("*"*80)
+    sys.stdout.flush() # To ensure that the print statements are immediately visible in the terminal while the server is running, you can explicitly flush the output buffer after each print statement using sys.stdout.flush().
+#________________________________________________________________________________________________________
+# Function to check the user input, and according to it select the output string 
+def check_user_input(request):
+    # Check the limitation counter for hard game
+    if 'limit' in request.session :
+        if (request.session['limit'] > 0):
+            request.session['limit'] -= 1
+    request.session['tries'] += 1                                                         # Increment the number of visits by spcific number
+    request.session['user_number'] = int(request.POST['user_number'])                     # Convert the number to integer first
+    if (request.session['number']- 10 <= request.session['user_number'] < request.session['number'] ):    # Case 2: too close but lower than number by 10 or less
+        request.session['result'] = 'Low'
+    elif (request.session['number']  < request.session['user_number'] <= request.session['number'] + 10): # Case 3: too close but higher than number by 10 or less
+        request.session['result'] = 'High'
+    elif (request.session['user_number'] > request.session['number']):                            # Case 4: too far from number from above 
+        request.session['result'] = 'Too high !'
+    else:                                                                         # Case 5: too far from number from below
+        request.session['result'] = 'Too low !'
+#________________________________________________________________________________________________________
+# Function to clear all data on session except winners list 
+def clear_without_removeing_winner_list(request):
+    # Store the winner list to keep it
+    item_to_keep = request.session.get('winners', None)
+    # Clear all data from the session
+    request.session.clear()
+    # Set the retained item back into the session
+    if item_to_keep is not None:
+        request.session['winners'] = item_to_keep
+#________________________________________________________________________________________________________
+# Function to add new winner to winners list
+def add_update_winner_information(request):
+    winners = request.session['winners']
+    name = request.POST['winner_name']
+    # Add new winner to the winners list
+    if (name not in request.session['winners']):
+        win_record = {'name':name,'tries':request.session['tries']}
+        winners.append(win_record)
+        request.session['winners'] = winners
+#________________________________________________________________________________________________________
+# Function to sort the winners list based on the number of tries
+def sort_winners(request):
+    winners = request.session['winners']
+    winners.sort(key=lambda x: x['tries'])
+    request.session['winners'] = winners
+#________________________________________________________________________________________________________
+# Function to remove current game information in order to start new game
+def start_new_game(request):
+    del request.session['limit']
+    del request.session['number']
+    del request.session['tries']
--- djangoPy3Env/great_number_game/myapp/views.py
+++ djangoPy3Env/great_number_game/myapp/views.py
@@ -1,16 +1,9 @@
 from django.shortcuts import render, redirect
-import sys
-import random # import the random module
+from . import models
 
 # This page used to select the game difficulty  
 def welcome(request):
-    if 'number' not in request.session :                    # Check if 'number' key exists in session
-        request.session['number'] = random.randint(1, 100) 	# random number between 1-100
-    if 'tries' not in request.session :                     # Check if 'tries' key exists in session
-        request.session['tries'] = 0
-    print(request.session['number'])
-    print("*"*80)
-    sys.stdout.flush() # To ensure that the print statements are immediately visible in the terminal while the Flask server is running, you can explicitly flush the output buffer after each print statement using sys.stdout.flush().
+    models.initialize_session(request)
     return render(request,"welcome.html")
 
 # Easy game
@@ -19,32 +12,12 @@ def easy(request):
 
 # Hard game
 def hard(request):
-    if 'winners' not in request.session:
-        request.session['winners'] = []
-    if 'limit' not in request.session :                     # Check if 'limit' key exists in session
-        request.session['limit'] = 5
-        request.session['type'] = 'hard'
-    print(request.session)
-    print("*"*80)
-    sys.stdout.flush() # To ensure that the print statements are immediately visible in the terminal while the Flask server is running, you can explicitly flush the output buffer after each print statement using sys.stdout.flush().
+    models.initialize_session_hard_game(request)
     return render(request,"hard.html",{'limit':request.session['limit']})
 
 def check_number(request):
     if (request.method == 'POST'):
-        # Check the limitation counter for hard game
-        if 'limit' in request.session :
-            if (request.session['limit'] > 0):
-                request.session['limit'] -= 1
-        request.session['tries'] += 1                                                         # Increment the number of visits by spcific number
-        request.session['user_number'] = int(request.POST['user_number'])                     # Convert the number to integer first
-        if (request.session['number']- 10 <= request.session['user_number'] < request.session['number'] ):    # Case 2: too close but lower than number by 10 or less
-            request.session['result'] = 'Low'
-        elif (request.session['number']  < request.session['user_number'] <= request.session['number'] + 10): # Case 3: too close but higher than number by 10 or less
-            request.session['result'] = 'High'
-        elif (request.session['user_number'] > request.session['number']):                            # Case 4: too far from number from above 
-            request.session['result'] = 'Too high !'
-        else:                                                                         # Case 5: too far from number from below
-            request.session['result'] = 'Too low !'
+        models.check_user_input(request)
         return redirect('/show_result')
            
 # show result (because if I use render-template in check-number function it will decrease the numbers when we refresh the page)
@@ -57,71 +30,29 @@ def show(request):
         if (request.session['user_number'] == request.session['number']):
             return render(request,"success.html",context)
         else:
-            return render(request,"limit_fail.html",context) # Fail case (hard game)
+            return render(request,"limit_fail.html",context)           # Fail case (hard game)
     if (request.session['user_number'] == request.session['number']):  # Case 1: Success (easy game)
         return render(request,"success.html",context)
     else:
-        return render(request,"fail.html",context)            # Fail case (easy game)
+        return render(request,"fail.html",context)                     # Fail case (easy game)
     
 # Clear the session 
 def clear_session(request):
-    # Store the winner list to keep it
-    item_to_keep = request.session.get('winners', None)
-    # Clear all data from the session
-    request.session.clear()
-    # Set the retained item back into the session
-    if item_to_keep is not None:
-        request.session['winners'] = item_to_keep
+    models.clear_without_removeing_winner_list(request)
     return redirect("/")
 
-# Leaderbord 
+# Leaderbord page
 def leaderbord(request):
     if (request.method == 'POST'):
-        winners = request.session['winners']
-        name = request.POST['winner_name']
-        print(name)
-        print("*"*80)
-        sys.stdout.flush()
-        if (name not in request.session['winners']):
-            print("ADD new one")
-            sys.stdout.flush()
-            win_record = {'name':name,'tries':request.session['tries']}
-            
-            winners.append(win_record)
-            request.session['winners'] = winners
-            # session['winners'] += (win_record)
-            print ("NEW LIST :")
-            print(request.session['winners'])
-            sys.stdout.flush()
-        else: 
-            print("Update if need")
-            sys.stdout.flush()
-            if request.session['winners'][name] > request.session['tries']:
-                request.session['winners'][name] =  request.session['tries']
-            print ("Updated LIST :")
-            print(request.session['winners'])
-            sys.stdout.flush()
+        models.add_update_winner_information(request)
         return redirect("/show_leaderboard" )
     
 # Show Leaderbord 
 def show_leaderbord(request):
-    # Sort the winners list based on the number of tries
-    winners = request.session['winners']
-    winners.sort(key=lambda x: x['tries'])
-    return render(request,"leaderboard.html", {'winners' : winners})
+    models.sort_winners(request)
+    return render(request,"leaderboard.html", {'winners' : request.session['winners']})
 
 # Go back to home
 def back_home(request):
-    del request.session['limit']
-    del request.session['number']
-    del request.session['tries']
+    models.start_new_game(request)
     return redirect('/')
-
-
-
-    
-
-
-
-
-
